3adf974a4f4306560c469d3ab3dae6a68eb207e9
gohilvaibhav88 <gohilvaibhav8844@gmail.com>
2024-01-03 21:59:57 +0530

    Added

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError = require('./utils/ExpressError.js')
 const {listingSchema} = require('./schema.js')
 const Review = require('./models/review.js')
-const listings = require("./routes/listing.js")
-const reviews = require('./routes/review.js')
+const userRouter = require('./routes/user.js');
+const listingRouter = require("./routes/listing.js")
+const reviewRouter = require('./routes/review.js')
 const session = require('express-session');
 const flash = require('connect-flash')
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Airbnb";
 
@@ -57,14 +62,36 @@ app.get("/", function(req, res){
 
 app.use(session(sessionOptions));
 app.use(flash());
+passport.initialize();
+passport.session();
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
 
 app.use((req, res, next)=>{
     res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
     next();
 })
 
-app.use('/listings' , listings);
-app.use('/listings/:id/reviews' , reviews);
+app.get('/demouser' , async (req, res)=>{
+    let fakeUser = new User({
+        email:"abcwwww@gmail.com",
+        username: "abcfffff",
+    })
+    let newUser = await User.register(fakeUser , "HelloWorld");
+    res.send(newUser);
+
+})
+
+app.use('/listings' , listingRouter);
+app.use('/listings/:id/reviews' , reviewRouter);
+app.use('/',userRouter);
 
 
 
